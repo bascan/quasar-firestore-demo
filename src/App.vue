1a72a37e0cf03a2397a0506cada02d86af76d259
Bas Cancrinus <bas@bascan.eu>
2018-11-29 12:48:12 +0800

    Added Firestore packages, initialization logic and don't open the browser window automatically.

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,37 @@
 </template>
 
 <script>
+import firebase from 'firebase'
+import 'firebase/firestore'
+import firebaseApp from 'firebase/app'
+
 export default {
-  name: 'App'
+  name: 'App',
+  preFetch ({ store }) {
+    // Initialize Firebase
+    const fierbaseConfig = {
+      apiKey: 'AIzaSyDGytmB6E1reBhCAMjA3kA9w5JfJPTXao4',
+      authDomain: 'job-cruizer.firebaseapp.com',
+      databaseURL: 'https://job-cruizer.firebaseio.com',
+      projectId: 'job-cruizer',
+      storageBucket: 'job-cruizer.appspot.com',
+      messagingSenderId: '689413510386'
+    }
+
+    firebaseApp.initializeApp(fierbaseConfig)
+    const $db = firebase.firestore()
+    // Firebase requirement
+    $db.settings({
+      timestampsInSnapshots: true
+    })
+    // Put firebase functionality into
+    // state for future access
+    Object.assign(store.state, {
+      $db: firebase.firestore(),
+      $firebase: firebaseApp
+    })
+  }
 }
 </script>
 
-<style>
-</style>
+<style></style>
