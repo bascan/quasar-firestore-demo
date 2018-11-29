5e2e81a7f4230b315e1c02883b46529b941cd8a0
Bas Cancrinus <bas@bascan.eu>
2018-11-29 17:06:58 +0800

    Added store, log Firestore query object.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,13 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    <q-list>
+      <q-list-header>
+        Jobs
+      </q-list-header>
+      <q-item v-for="(job, idx) in jobs" :key="idx">
+        <q-item-main :label="job.title"/>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
@@ -9,6 +16,17 @@
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'JobList',
+  data () {
+    return {
+      jobs: []
+    }
+  },
+  created () {
+    this.$store.state.$db.collection('jobs').orderBy('from').onSnapshot(
+      (next) => console.log(next),
+      (error) => console.log(error)
+    )
+  }
 }
 </script>
